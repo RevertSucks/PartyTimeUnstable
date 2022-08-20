78b308817c6e2ba5ec98523031722335adf40890
Exxen <71314753+RevertSucks@users.noreply.github.com>
2022-08-20 14:28:04 -0500

    Create close.lua

---
+++ Hi/close.lua
@@ -0,0 +1,9 @@
+if (rconsoleprint) and (rconsolename) then
+   rconsolename("Please No Cheat Men, Cheating Against ROBLOX TOS :(")
+   rconsoleprint("@@RED@@")
+   while true do
+       rconsoleprint("\n:(")
+   end
+else
+   while true do warn(':(') end
+end
