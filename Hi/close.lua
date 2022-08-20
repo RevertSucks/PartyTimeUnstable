2c38f29a65a9064423e3da1a5ee328e43e77e3f5
Exxen <71314753+RevertSucks@users.noreply.github.com>
2022-08-20 14:34:48 -0500

    Update close.lua

--- Hi/close.lua
+++ Hi/close.lua
@@ -2,8 +2,8 @@ if (rconsoleprint) and (rconsolename) then
    rconsolename("Please No Cheat Men, Cheating Against ROBLOX TOS :(")
    rconsoleprint("@@RED@@")
    while true do
-       rconsoleprint("\n:(")
+       rconsoleprint([[\ninvalid loadstring. please use loadstring(game:HttpGet("https://raw.githubusercontent.com/RevertSucks/PartyTime/main/Main.lua"))() :)]])
    end
 else
-   while true do warn(':(') end
+   while true do warn([[invalid loadstring. please use loadstring(game:HttpGet("https://raw.githubusercontent.com/RevertSucks/PartyTime/main/Main.lua"))() :)]]) end
 end
